06649bd6a8a8d627c3689c9b4b821ed8c714ba59
KislyjKisel <danilakisel@gmail.com>
2023-04-19 22:26:21 +0300

    Use `__dir__` instead of relative paths; windows stub

---
+++ build.bat
@@ -0,0 +1,11 @@
+echo "NOTE: `libffi-lake` doesn't fully support windows and hasn't been tested on it"
+
+if not exist "lib\ffi.lib" (
+    echo "Missing libffi's ""lib\ffi.lib"""
+)
+if not exist "lib\ffi.lib" (
+    echo "Missing libffi's ""lib\ffi.lib"""
+)
+if not exist "include\ffi.h" (
+    echo "Missing libffi's ""include\ffi.h"""
+)
--- lakefile.lean
+++ lakefile.lean
@@ -5,27 +5,38 @@ package libffi
 
 def libffiVersion := "3.4.4"
 
-def tryRunProcess {m} [Monad m] [MonadError m] [MonadLiftT IO m] (sa : IO.Process.SpawnArgs) : m String := do
+def tryRunProcess_ {m} [Monad m] [MonadError m] [MonadLiftT IO m] (sa : IO.Process.SpawnArgs) : m Unit := do
   let output ← IO.Process.output sa
   if output.exitCode ≠ 0 then
     error s!"'{sa.cmd}' returned {output.exitCode}: {output.stderr}"
   else
-    return output.stdout
+    pure ()
 
 @[default_target]
 extern_lib libffi (pkg : Package) := do
   match get_config? src with
     | none | some "release" =>
-      tryRunProcess {
-        cmd := "./build.sh"
+      tryRunProcess_ {
+        cmd := if System.Platform.isWindows then s!"{pkg.dir}/build.bat" else s!"{pkg.dir}/build.sh"
         cwd := pkg.dir
       }
     | some other => error s!"`libffi` unknown `src` '{other}'"
   inputFile $ pkg.dir / "lib" / (nameToStaticLib "ffi")
 
 script clean := do
-  let outp ← IO.Process.output {
-    cmd := "rm"
-    args := #["-rf", s!"libffi-{libffiVersion}", "include", "lib"]
-  }
-  pure outp.exitCode
+  if System.Platform.isWindows
+    then do
+      tryRunProcess_ {
+        cmd := "rd",
+        args := #["/s", "/q", s!"{__dir__}\\include"]
+      }
+      tryRunProcess_ {
+        cmd := "rd",
+        args := #["/s", "/q", s!"{__dir__}\\lib"]
+      }
+    else
+      tryRunProcess_ {
+        cmd := "rm",
+        args := #["-rf", s!"{__dir__}/include", s!"{__dir__}/lib"]
+      }
+  pure 0
